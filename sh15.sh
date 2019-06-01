2da60366d4c0648906380176e863ac566adbfbb9
chengcainanjing <chengcainuist@gmail.com>
2019-06-01 15:29:04 +0800

    添加for 固定循环脚本

---
+++ sh15.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Program:
+#	Use for ... loop to print 3 animals.
+# History:
+# 2019.06.01 chengcai	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+for animal in dog cat elephant
+do
+        echo "There are ${animal}s..."
+done
+
+
+
+
+
+
+
+
+
