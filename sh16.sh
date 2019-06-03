19ce916c63a6cc68f03097dc0da0574d77669440
chengcainanjing <chengcainuist@gmail.com>
2019-06-03 12:34:07 +0800

    更新16sh，测试可用

--- sh16.sh
+++ sh16.sh
@@ -3,8 +3,13 @@
 #	Use id, finger command to check system account's information.
 # History:
 # 2019.06.01 chengcai	First release
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+#test ok in Linux-centos7
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/etc/selinux/targeted/active/modules/100/finger
 export PATH
 
-users=$(cut -d ':' -f1 finger /etc/passwd)     #获取账号名称
+users=$(cut -d ':' -f1  /etc/passwd)     #获取账号名称
 for username in $users                  #开始循环进行
+do
+    id $username
+    finger $username
+done
