9e745aca046a3f79f97f29970366b9f7c46d10e3
chengcainanjing <chengcainuist@gmail.com>
2019-06-15 10:18:36 +0800

    更新创建读写文件描述符脚本

--- sh25.sh
+++ sh25.sh
@@ -8,27 +8,4 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 echo "-----------------------创建读写文件描述符----------------------"
-exec 6<&0                   #文件描述符6表示保存STDIN的位置，该行表示将屏幕输入重定向到文件输入
-exec 0<sh24input.file       #将STDIN输入重定向到文件中
-
-count=1
-while read line
-do
-    echo "Line #$count : $line"
-    count=$[ count + 1 ]
-done
-
-exec 0<&6                   #恢复由文件输入，改为由屏幕输入
-
-read -p "Are you done now?" answer
-case $answer in
-    y|Y)
-        echo "Goodbye"
-        ;;
-    n|N)
-        echo "Sorry, this is the end."
-        ;;
-    *)
-        echo "Please input 'y|Y' or 'n|N'"
-        ;;
-esac
+#暂时没写，在《Linux命令行与shell脚本编程大全》第320页
