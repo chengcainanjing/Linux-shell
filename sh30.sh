6f602a6a9a23b23752942be356561c7f646c5f85
chengcainanjing <chengcainuist@gmail.com>
2019-06-15 14:48:25 +0800

    捕获信号，更新脚本
    (cherry picked from commit 19ce916c63a6cc68f03097dc0da0574d77669440)

--- sh30.sh
+++ sh30.sh
@@ -7,6 +7,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+echo "-----------------------捕获信号----------------------"
 trap "echo 'Sorry! I have trapped ctrl-c'" SIGINT
 
 echo "This is a test script."
