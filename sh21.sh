ec20ca3f4be7349a35f8c316340a4928b2e9b48f
chengcainanjing <chengcainuist@gmail.com>
2019-06-17 08:59:22 +0800

    更新代码

--- sh21.sh
+++ sh21.sh
@@ -8,12 +8,12 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 echo "-----------------------输入重定向----------------------"
-exec 0< sh21input.file              #exec命令允许将STDIN重定向到Linux系统上的文件中
+#exec 0< sh21input.file              #exec命令允许将STDIN重定向到Linux系统上的文件中
 count=1
 
 while read line; do                 #read line 是对于输入的文档进行逐行读取
     echo  "Line #$count: $line"
     count=$[ $count + 1 ]
-done
-
+#done
+done < sh21input.file        #此行代码可以替换exec 0< sh21input.file与done。
 echo "It is finished."
